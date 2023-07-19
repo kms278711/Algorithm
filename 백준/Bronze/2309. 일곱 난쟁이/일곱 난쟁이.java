259ce62a043dcc143dd0e8dc5a826c7fab483983
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-19 11:56:09 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =  null;
+		int answer[] = new int[7];
+		int dwarf[] = new int[9];
+		int sum = 0;
+		int idx = 0;
+		// 총합 구하기
+		for (int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
+			int length = Integer.parseInt(st.nextToken());
+			sum += length;
+			dwarf[i] = length;
+		}
+		// lt rt로 가짜 찾기
+		for (int lt = 0; lt < dwarf.length-1; lt++) {
+			for (int rt = lt+1; rt < dwarf.length; rt++) {
+				if(sum-dwarf[lt]-dwarf[rt] == 100) {
+					sum = 100;
+					dwarf[lt] = 0;
+					dwarf[rt] = 0;
+					break;
+				}
+			}
+			if(sum == 100) break;
+		}
+		for (int i = 0; i < dwarf.length; i++) {
+			if(dwarf[i] !=0 ) {
+				answer[idx] = dwarf[i];
+				idx++;
+			}
+		}
+		Arrays.sort(answer);
+		for (int i = 0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
+	}
+}
