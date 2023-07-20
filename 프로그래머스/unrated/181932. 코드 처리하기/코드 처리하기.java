8bd06ff4d8722c25151077a0d36f09c8f0efa275
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-20 21:36:34 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        char[] arr = code.toCharArray();
+        for (int idx = 0; idx < arr.length; idx++) {
+        	if(mode==1) {
+        		if(arr[idx] != '1') {
+        			if(idx%2 == 1) answer+=arr[idx];
+        		} else mode = 0;	
+        	} else {
+        		if(arr[idx] != '1') {
+        			if(idx%2 == 0) answer+=arr[idx];
+        		} else mode = 1;	
+        	}
+		}
+        if(answer.equals("")) answer = "EMPTY";
+        return answer;
+    }
+}
