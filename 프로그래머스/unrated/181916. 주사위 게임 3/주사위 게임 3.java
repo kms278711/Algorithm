cdb4e1784fb537ed402f5c31aa3dfcbb8a701977
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-20 22:43:37 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        int p = 0;
+        int q = 0;
+        int r = 0;
+        int[] used = new int[7];
+        //카운팅
+        used[a] += 1;
+        used[b] += 1;
+        used[c] += 1;
+        used[d] += 1;
+       
+        //검사
+        for (int i = 1; i <= 6; i++) {
+			if(used[i] == 4) {
+				answer = 1111*i;
+				break;
+			} else if(used[i] == 3) {
+				p = i;
+				for (int j = 1; j <= 6; j++) {
+					if(used[j] ==1) {
+						q = j;
+						break;
+					}
+				}
+				answer =(int)Math.pow(10*p+q, 2);
+				break;
+			} else if(used[i] == 2) {
+				p = i;
+				for (int j = 1; j <= 6; j++) {
+					if(j!=i && used[j]==2) {
+						q = j;
+						answer = (p+q)*Math.abs(p-q);
+						break;
+					} else if(used[j] == 1) {
+						q = j;
+						for (int k = 1; k <= 6; k++) {
+							if(k!=j && used[k]==1) {
+								r = k;
+								break;
+							}
+						}
+						answer = q*r;
+						break;
+					}
+				}
+				break;
+			}
+			if(i==6) {
+				for (int j = 1; j <= 6; j++) {
+					if(used[j] == 1) {
+						answer = j;
+						break;
+					}
+				}
+			}
+        }
+        
+        return answer;
+    }
+}
