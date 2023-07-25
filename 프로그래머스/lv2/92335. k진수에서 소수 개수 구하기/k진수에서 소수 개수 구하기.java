5988ef690cb81993fcf74cbe0179608b2ec153e0
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-25 23:40:03 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 12.23 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int solution(long n, int k) {
         int answer = 0;
+        
+        //진수변환
         String num = Long.toString(n, k);
+        
         int idx = 0;
         char[] c = num.toCharArray();
         while(idx<num.length()+1) {
         	String tmp = "";
+        	//소수인지 확인
         	boolean flag = false;
+        	//0전까지 숫자 구하기
         	while(true) {
         		if(idx==num.length()) break;
         		if(c[idx] == '0') {
@@ -18,6 +27,7 @@ class Solution {
         	}
         	if(tmp != "") {
 	        	long prime = Long.parseLong(tmp);
+	        	// 소수인지 검사
 	        	if(prime != 1) {
 		        	for (int i = 2; i <= Math.sqrt(prime); i++) {
 						if(prime%i==0) {
