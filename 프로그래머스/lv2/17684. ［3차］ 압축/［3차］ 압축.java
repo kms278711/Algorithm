82af86291873aa30c0d9ea6660653e31058a1381
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-25 23:33:42 +0900

    [level 2] Title: [3차] 압축, Time: 5.79 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -5,9 +5,17 @@ import java.util.List;
 class Solution {
     public Integer[] solution(String msg) {
         Integer[] answer = {};
+        
+        //사전리스트
         List<String> arr = new ArrayList<>();
+        
+        //정답리스트
         List<Integer> answerArr = new ArrayList<>();
+        
+        //첫인덱스 채우기
         arr.add(" ");
+        
+        //A~Z 사전에 넣기
         for (int i = 0; i < 26; i++) {
 			arr.add(Character.toString((char)(65+i)));
 		}
@@ -15,17 +23,23 @@ class Solution {
         int idx = 0;
         while(true) {
         	if(idx==msgArr.length)break;
+        	
+        	//현재입력 index
             int tmp = 0;
+            
             String str = msgArr[idx];
 			while(true) {
 				if(arr.contains(str)) {
-					System.out.println(str);
 					tmp = arr.indexOf(str);
 					idx++;
+					
+					//마지막인지 체크
 					if(idx==msgArr.length) {
 						answerArr.add(tmp);
 						break;
 					}
+					
+					//문자 더하기
 					str += msgArr[idx];
 				} else {
 					arr.add(str);
