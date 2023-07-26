7bcb500841454234b78409203d6f69aa779009f3
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-26 22:43:55 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -1,9 +1,10 @@
 class Solution {
     public String[] solution(String[] quiz) {
-    	System.out.println(quiz.length);
         String[] answer = new String[quiz.length];
         for (int i = 0; i < quiz.length; i++) {
+        	// 뛰어쓰기 기준으로 나눠서 배열생성
 			String[] elements = quiz[i].split(" ");
+			//연산 확인
 			if(elements[1].equals("-")) {
 				if(Integer.parseInt(elements[4]) == Integer.parseInt(elements[0]) - Integer.parseInt(elements[2]))answer[i] = "O";
 				else answer[i] = "X";
