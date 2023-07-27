2fc3f638981ced87295564169244a820c7aaec2a
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-07-27 22:17:12 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -4,6 +4,7 @@ class Solution {
         int[] nArr = new int[101];
         int cur = 1;
         for (int i = 1; i < nArr.length; i++) {
+            //3의 배수이거나 3이 포함되어있으면 현재 숫자 증가
         	while(cur%3 == 0 || Integer.toString(cur).contains("3")) {
         		cur++;
         	}
