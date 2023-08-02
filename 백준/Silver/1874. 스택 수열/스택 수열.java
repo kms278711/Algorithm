107e533db43fafa68791a95b52f20ab4c9995440
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-08-02 09:16:53 +0900

    [Silver II] Title: 스택 수열, Time: 376 ms, Memory: 27912 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -6,34 +6,39 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
+		//input 배열
 		int[] arr = new int[n];
+		//스택생성
 		Stack<Integer> stack = new Stack<>();
 		//현재 값
 		int cur = 1;
 		//비교할 정답 인덱스
 		int idx = 0;
-		stack.push(cur++);
-		sb.append("+\n");
+		//입력받기
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
+		
 		while(true) {
+			//비어있다면 값 넣기
 			if(stack.isEmpty()) {
 				stack.push(cur++);
 				sb.append("+\n");
 			} else {
+				//제일 위에있는 값 비교
 				if(stack.peek() == arr[idx]) {
 					idx++;
 					stack.pop();
 					sb.append("-\n");
 				} else {
+					//peek값이 아니면서 현재값을 넘어간경우
 					if(cur == n+1) break;
 					stack.push(cur++);
 					sb.append("+\n");
 				}
 			}
+			//마지막 input 인덱스까지 탐색한경
 			if(idx==n) break;
-			if(stack.isEmpty() && cur==n+1) break;
 		}
 		if(stack.isEmpty() && cur==n+1) System.out.println(sb);
 		else System.out.println("NO");	
