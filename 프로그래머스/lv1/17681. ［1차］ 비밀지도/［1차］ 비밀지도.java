d117b69767cdf23780e30e22a179173e484f71d2
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-08-04 15:01:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,47 +1,22 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        int[][] map1 = new int[n][n];
-        int[][] map2 = new int[n][n];
-        int[][] answerMap = new int[n][n];
-        int idx = 0;
-        
-        for(int x=0; x<n; x++) {
-            int tmp = arr1[x];         
-            for(int k=n-1; k>=0; k--) {
-                if(tmp >= Math.pow(2, k)) {
-                    map1[x][idx] = 1;
-                    tmp = tmp % (int)Math.pow(2, k);
-                } 
-                idx ++;
-            }
-            idx = 0;
-        }
-        
-        for(int x=0; x<n; x++) {
-            int tmp = arr2[x];         
-            for(int k=n-1; k>=0; k--) {
-                if(tmp >= Math.pow(2, k)) {
-                    map2[x][idx] = 1;
-                    tmp = tmp % (int)Math.pow(2, k);
-                } 
-                idx ++;
-            }
-            idx = 0;
-        }
-        
-        String str = "";
-        for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(map1[i][j] == 1 | map2[i][j] == 1) {
-					str += "#";
-				} else if(map1[i][j] == 0 && map2[i][j] == 0) {
-					str += " ";
+	public String[] solution(int n, int[] arr1, int[] arr2) {
+		//정답 배열
+		String[] answer = new String[n];
+		//계산해서 넣을 배열
+		for (int i = 0; i < n; i++) {
+			answer[i] = Integer.toBinaryString((arr1[i] | arr2[i]));
+		}
+		for (int i = 0; i < n; i++) {
+			String tmp = answer[i].replace("0", " ").replace("1", "#");
+			if(tmp.length()<n) {
+				//앞자리 공백 채워주기
+				int length = tmp.length();
+				for (int j = 0; j < n-length; j++) {
+					tmp = " " + tmp;
 				}
 			}
-			answer[i] = str;
-			str = "";
+			answer[i] = tmp;
 		}
-        return answer;
-    }
+		return answer;
+	}
 }
