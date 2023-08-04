e71207661d5be8e2ade4195e7bc3ab5be3ea972a
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-08-04 15:56:16 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.44 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 class Solution {
     public int solution(String str1, String str2) {
     	int answer = 0;
+        //소문자로 통일
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
         char[] arr1 = str1.toCharArray();
@@ -10,19 +11,26 @@ class Solution {
         ArrayList<String> strList1 = new ArrayList<>();
         ArrayList<String> strList2 = new ArrayList<>();
         for (int i = 0; i < arr1.length-1; i++) {
+            //문자아닌경우 continue
         	if(!Character.isAlphabetic(arr1[i]) || arr1[i+1] == ' ' || !Character.isAlphabetic(arr1[i+1])) continue;
+            //두자리 뽑기
         	String tmp = Character.toString(arr1[i]) + Character.toString(arr1[i+1]);
         	strList1.add(tmp);
         }
+        //마찬가지
         for (int i = 0; i < arr2.length-1; i++) {
         	if(!Character.isAlphabetic(arr2[i]) || arr2[i+1] == ' ' || !Character.isAlphabetic(arr2[i+1])) continue;
         	String tmp = Character.toString(arr2[i]) + Character.toString(arr2[i+1]);
         	strList2.add(tmp);
         }
 
+        
         if(strList1.size()+strList2.size() == 0) return 65536;
+        //소수 계산을 위해 double
+        // 합집합 계산
         double B = strList1.size()+strList2.size();
         
+        //교집합 개수
         double A = 0;
         for (String s: strList1) {
 			if(strList2.contains(s)) {
@@ -30,7 +38,9 @@ class Solution {
 				strList2.remove(strList2.indexOf(s));
 			}
 		}
+        //합집합
         B -= A;
+        //유사도 계산
         double tmp = A/B;
         answer = (int)(Math.floor(tmp * 65536));
         return answer;
