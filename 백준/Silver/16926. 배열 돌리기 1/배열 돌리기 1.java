cc155d4ae73a820070368644f61f0a3ead5ae398
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-08-08 16:15:13 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 1796 ms, Memory: 303948 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -9,6 +9,8 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int R = Integer.parseInt(st.nextToken());
 		int[][] arr = new int[N][M];
+		
+		//채우기
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine()); 
 			for (int j = 0; j < M; j++) {
@@ -17,10 +19,13 @@ public class Main {
 		}
 		for (int i = 0; i < R; i++) {
 			int[][] tmp = new int[N][M];
+			//x범위 dx[0]~dx[1]
 			int[] dx = new int[2];
+			//y범위 dy[0]~dy[1]
 			int[] dy = new int[2];
 			dx[1] = N-1;
 			dy[1] = M-1;
+			//범위의 차이가 1일때까지
 			while(dx[1]-dx[0] >= 1 && dy[1]-dy[0] >= 1) {
 				for (int j = dy[1]-1; j >= dy[0]; j--) {
 					tmp[dx[0]][j] = arr[dx[0]][j+1];
@@ -34,13 +39,14 @@ public class Main {
 				for (int j = dx[1]-1; j >= dx[0]; j--) {
 					tmp[j][dy[1]] = arr[j+1][dy[1]];
 				}
+				//다음 구역(중앙쪽으로)
 				dx[0] += 1;
 				dx[1] -= 1;
 				dy[0] += 1;
 				dy[1] -= 1;
 			}	
 			
-			//복사
+			//원본 덮어쓰기
 			for (int j = 0; j < N; j++) {
 				for (int k = 0; k < M; k++) {
 					arr[j][k] = tmp[j][k];
