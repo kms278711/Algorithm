88aa39605e6435c6395d47e70dcc7f595ece07ac
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-10-11 12:19:03 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 284 ms, Memory: 86,408 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,153 +1,129 @@
-import java.io.*;
 import java.util.*;
-
-//좌표 클래스
-class Point{
-	public int x,y;
-	
-	public Point(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-}
+import java.io.*;
 
 public class Solution {
-	//가로세로길이, 최대 멕시노스 개수, 최대개수일때  전선길이, 탐색해야할 총 개수
-	private static int N, maxCnt, maxSize, total;
-	private static int[][] map;
-	//상우하좌
-	private static int[] dx = {-1, 0, 1, 0};
-	private static int[] dy = {0, 1, 0, -1};
-	/**
-	 * 탐색
-	 * @param cur 해당분기 현재 맵 상태
-	 * @param cnt 연결코어수
-	 * @param size 연결선수
-	 */
-	private static void DFS(int[][] cur, int checkCnt, int cnt, int sX) {
-		//총개수에서 현재탐색개수 뺴고 연결된 개수가 지금까지 계산된 최대개수보다 작으면 탐색 패스
-		if(total-checkCnt+cnt < maxCnt) return;
-		//더이상 코어가 없다면
-		if(checkCnt == total) {
-			if(cnt < maxCnt) return;
-
-			int size = 0;
+	//cell들의 좌표 리스트
+	static List<int[]> cells;
+	//맵
+	static int[][] map;
+	//4방향
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	//맵길이, 최대 셀 개수일때 전선길이, 최대 셀 개수
+	static int N, answer, maxCell;
+	
+	//내부인지 확인
+	private static boolean isIn(int x, int y) {
+		return x>=0 && y>=0 && x<N && y<N;
+	}
+	
+	private static void find(int cnt, int[][] cur, int conCell) {
+		//더이상 진행할 필요없는 조건
+		//아직 탐색 안한 셀 개수 + 연결된 개수가 최대 셀 개수보다 작으면 진행 x
+		if(cells.size()-cnt+conCell < maxCell) return;
+		//모두 탐색했으면
+		if(cnt == cells.size()) {
+			//전선 길이 체크
+			int len = 0;
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if(cur[i][j] == -1) {
-						size++;
-					}
+					if(cur[i][j] == -1) len++;
 				}
 			}
-			
-			//최대로 나온 것보다 현재분기의 코어수가 더 많으면
-			if(cnt > maxCnt) {
-				maxCnt = cnt;
-				maxSize = size;
-			//코어수가 같다면
-			} else if(cnt==maxCnt) {
-				//더작은 전선수로 교체
-				maxSize = Math.min(maxSize, size);
+			//같은 경우 더 작은 값으로 갱신
+			if(maxCell == conCell) {
+				answer = Integer.min(len, answer);
+			//더 큰 셀 개수라면 셀 개수와 길이 갱신
+			} else {
+				maxCell = conCell;
+				answer = len;
 			}
+			return;
 		} else {
-			//현재 상태 복사 배열
-			int[][] tmp = new int[N][N];
-			//배열 복사
-			for (int h = 0; h < N; h++) {
-				tmp[h] = Arrays.copyOf(cur[h], N);
+			//현재 탐색 셀
+			int[] curCell = cells.get(cnt);
+			//현재상태 복사할 임시 맵
+			int[][] mapTmp = new int[N][N];
+			//복사
+			for (int j = 0; j < N; j++) {
+				mapTmp[j] = Arrays.copyOf(cur[j], N);
 			}
-			for (int i = sX; i < N; i++) {
+			//1. 연결 안할 경우
+			find(cnt+1, mapTmp, conCell);
+			
+			//2. 4방향 체크 후 갈 수 있는 곳으로 연결하는 경우
+			for (int i = 0; i < 4; i++) {
+				//현재 상태로 초기화
 				for (int j = 0; j < N; j++) {
-					//해당 칸에 코어가 있을 경우
-					if(cur[i][j] == 1) {
-						//현재 좌표 입력
-						int x = i, y = j;
-						//탐색한 코어 2로 표시
-						cur[i][j] = 2;
-						tmp[i][j] = 2;
-						//4방향 확인
-						for (int k = 0; k < 4; k++) {
-							//초기화
-							x = i;
-							y = j;
-							int cntTmp = 0;
-							//한방향으로 쭉 가보기
-							while(true) {
-								x += dx[k];
-								y += dy[k];
-								if(isOut(x, y)) {
-									cntTmp ++;
-									break;
-								} else if(tmp[x][y] == 0) {
-									tmp[x][y] = -1;
-								} else {
-									//원상복귀
-									if(dx[k] == 0) {
-										for (int l = 0; l < N; l++) {
-											tmp[i][l] = cur[i][l];
-										}
-									} else {
-										for (int l = 0; l < N; l++) {
-											tmp[l][j] = cur[l][j];
-										}	
-									}
-									break;
-								}
-							}
-							//추가된 코어수만큼, 추가된 연결선수만큼 더해서 다음분기로
-							DFS(tmp, checkCnt+1, cnt+cntTmp, i);
-							if(dx[k] == 0) {
-								for (int l = 0; l < N; l++) {
-									tmp[i][l] = cur[i][l];
-								}
-							} else {
-								for (int l = 0; l < N; l++) {
-									tmp[l][j] = cur[l][j];
-								}	
-							}
-						}
-						//탐색 안했다고 표시
-						cur[i][j] = 1;
+					mapTmp[j] = Arrays.copyOf(cur[j], N);
+				}
+				
+				//다음좌표 계산을 위해 현재좌표로 초기화
+				int nx = curCell[0];
+				int ny = curCell[1];
+				//해당 방향 가능 여부
+				boolean isPossible = false;
+				while(true) {
+					//다음 좌표 값 계산
+					nx += dx[i];
+					ny += dy[i];
+					//밖으로 나갔다면 연결 가능
+					if(!isIn(nx, ny)) {
+						isPossible = true;
+						break;
+					//장애물이 있을 경우 탐색 종료
+					} else if(mapTmp[nx][ny] != 0){
+						break;
 					}
+				}
+				
+				//연결이 가능하다면
+				if(isPossible) {
+					//다음좌표 계산을 위해 현재좌표로 초기화
+					nx = curCell[0];
+					ny = curCell[1];
+					
+					while(true) {
+						nx += dx[i];
+						ny += dy[i];
+						//해당방향 끝까지 배열에 전설을 -1로 표시하여 연결
+						if(!isIn(nx,ny)) break;
+						mapTmp[nx][ny] = -1;
+					}
+					//현재 맵 상태와 셀 하나 연결되었다고 표시 후, 다음 셀로 이동
+					find(cnt+1, mapTmp, conCell+1);
 				}
 			}
 		}
 	}
 	
-	//밖으로 나갔는지(연결됐는지)확인
-	private static boolean isOut(int x, int y) {
-		if(x>=N || y>=N || x<0 || y<0) return true;
-		return false;
-	}
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		//입력 및 초기화
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= T; i++) {
-			//입력 및 초기화
-			sb.append("#").append(i).append(" ");
+		for(int i=1; i <= T; i++) {
+			sb.append("#" + i + " ");
+			answer = Integer.MAX_VALUE;
+			maxCell = Integer.MIN_VALUE;
 			N = Integer.parseInt(br.readLine());
+			cells = new ArrayList<>();
 			map = new int[N][N];
-			maxCnt = 0;
-			total = 0;
-			
-			maxSize=Integer.MAX_VALUE;
 			for (int j = 0; j < N; j++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int k = 0; k < N; k++) {
 					map[j][k] = Integer.parseInt(st.nextToken());
-					//외곽에 있는것 연결되어 있기떄문에 탐색영역 제외하기
-					if(map[j][k] == 1 && (j==0 || k==0 || j==N-1 || k==N-1)) {
-						map[j][k] = 2;
+					if(map[j][k] == 1) {
+						//가장자리에 있는 것들 탐색대상에서 제외
+						if(!(j==0 || j==N-1 || k==0 || k==N-1)) cells.add(new int[] {j, k});
 					}
-					if(map[j][k] == 1) total++;
 				}
 			}
-			DFS(map, 0, 0, 0);
-			sb.append(maxSize).append("\n");
+			//탐색 진행
+			find(0, map, 0);
+			sb.append(answer).append("\n");
 		}
 		System.out.println(sb);
 	}
+	
 }
