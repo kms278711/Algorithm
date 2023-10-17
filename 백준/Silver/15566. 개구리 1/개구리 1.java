f6cf5d18a8756b65d06e33354ff777f00ad5ef66
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-10-17 09:33:27 +0900

    [Silver I] Title: 개구리 1, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/15566. 개구리 1/개구리 1.java
+++ 백준/Silver/15566. 개구리 1/개구리 1.java
@@ -2,13 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	//정답비교를 위해 NO로 초기화
 	private static String answer="NO";
+	//개구리마리수, 통나무개수
 	private static int N,M;
 	//주제별 흥미
 	private static int[][] talk;
-	//좋아하는 연꽃
+	//좋아하는 연꽃정보 [개구리번호][선호연꽃 2개]
 	private static int[][] favor;
-	//연꽃에 게구리 번호 세팅정보
+	//연꽃에 개구리 번호 세팅정보
 	private static int[] result;
 	//통나무 배열
 	private static Route[] routeArr;
@@ -24,21 +26,27 @@ public class Main {
 			this.end = end;
 			this.favor = favor;
 		}
-
 		@Override
 		public String toString() {
 			return "Route [start=" + start + ", end=" + end + ", favor=" + favor + "]";
 		}
-		
 	}
 	
+	//선호 연꽃을 기준으로 개구리 세팅하기
 	private static void find(int cnt) {
+		//이미 정답이 나왔다면 탐색 종료
 		if(answer.equals("YES")) return;
+		//1번부터 탐색 시작하기때문에 N+1이 종료 조건
 		if(cnt == N+1) {
+			//통나무 목록 돌면서 대화가능한지 체크
 			for (int i = 0; i < M; i++) {
+				//현재 통나무
 				Route cur = routeArr[i];
+				//주제의 관심도가 같은지 체크
+				//다르면 리턴
 				if(talk[result[cur.start]][cur.favor] != talk[result[cur.end]][cur.favor]) return;
 			}
+			//for문을 나왔다면 YES로 갱신해주고 출력
 			answer = "YES";
 			System.out.println(answer);
 			if(answer.equals("YES")) {
@@ -48,10 +56,15 @@ public class Main {
 			}
 			return;
 		} else {
+			//연꽃개수만큼 탐색
 			for (int i = 0; i < 2; i++) {
+				//cnt번쨰 개구리의 선호 연꽃이 비어있다면
 				if(result[favor[cnt][i]] == 0) {
+					//cnt번쨰 개구리의 선호 연꽃이 비어있다면 해당 개구리 넣기
 					result[favor[cnt][i]] = cnt;
+					//다음 개구리 세팅
 					find(cnt+1);
+					//세팅 복구
 					result[favor[cnt][i]] = 0;
 				}
 			}
@@ -64,6 +77,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		//개구리1번부터 시작
 		talk = new int[N+1][5];
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -71,7 +85,7 @@ public class Main {
 				talk[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+		//개구리1번부터 시작
 		favor = new int[N+1][2];
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -79,7 +93,6 @@ public class Main {
 				favor[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		result = new int[N+1];
 		routeArr = new Route[M];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -88,7 +101,9 @@ public class Main {
 			int favor = Integer.parseInt(st.nextToken());
 			routeArr[i] = new Route(start, end, favor);
 		}
+		result = new int[N+1];
 		find(1);
+		//정답이 여전히 NO라면 NO출력
 		if(answer.equals("NO")) System.out.println(answer);
 	}
 	
