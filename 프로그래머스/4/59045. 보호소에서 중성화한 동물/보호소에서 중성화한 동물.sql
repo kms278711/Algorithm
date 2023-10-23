35afb7c5b700b1dccbb5e655ed2f67fc9a7bd688
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-10-23 17:25:17 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,11 @@
 SELECT animal_id, animal_type, name
 from animal_ins
-where animal_id in (select animal_id from animal_outs
-where sex_upon_outcome not like 'Intact%'
-and animal_id in (select animal_id from animal_ins
-where sex_upon_intake like 'Intact%'));
+where animal_id in (
+    /* 나갈 때 중성화 되있던 동물목록 */
+    select animal_id from animal_outs
+    where sex_upon_outcome not like 'Intact%'
+    and animal_id in (
+        /* 들어올 때 중성화 거치지 않은 동물목록 */
+        select animal_id from animal_ins
+        where sex_upon_intake like 'Intact%')
+    );
