4c453007561b95ee721f87a00d3238b9fe653353
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-11-07 14:20:40 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
