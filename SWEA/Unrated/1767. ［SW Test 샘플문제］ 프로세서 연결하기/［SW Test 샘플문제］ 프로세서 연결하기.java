451198037c2edc750e8cc3c75abdd87ffd6e0774
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-11-15 16:00:01 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 138 ms, Memory: 22,388 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -2,128 +2,128 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-	//cell들의 좌표 리스트
-	static List<int[]> cells;
-	//맵
-	static int[][] map;
-	//4방향
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	//맵길이, 최대 셀 개수일때 전선길이, 최대 셀 개수
-	static int N, answer, maxCell;
-	
-	//내부인지 확인
-	private static boolean isIn(int x, int y) {
-		return x>=0 && y>=0 && x<N && y<N;
-	}
-	
-	private static void find(int cnt, int[][] cur, int conCell) {
-		//더이상 진행할 필요없는 조건
-		//아직 탐색 안한 셀 개수 + 연결된 개수가 최대 셀 개수보다 작으면 진행 x
-		if(cells.size()-cnt+conCell < maxCell) return;
-		//모두 탐색했으면
-		if(cnt == cells.size()) {
-			//전선 길이 체크
-			int len = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if(cur[i][j] == -1) len++;
-				}
-			}
-			//같은 경우 더 작은 값으로 갱신
-			if(maxCell == conCell) {
-				answer = Integer.min(len, answer);
-			//더 큰 셀 개수라면 셀 개수와 길이 갱신
-			} else {
-				maxCell = conCell;
-				answer = len;
-			}
-			return;
-		} else {
-			//현재 탐색 셀
-			int[] curCell = cells.get(cnt);
-			//현재상태 복사할 임시 맵
-			int[][] mapTmp = new int[N][N];
-			//복사
-			for (int j = 0; j < N; j++) {
-				mapTmp[j] = Arrays.copyOf(cur[j], N);
-			}
-			//1. 연결 안할 경우
-			find(cnt+1, mapTmp, conCell);
-			
-			//2. 4방향 체크 후 갈 수 있는 곳으로 연결하는 경우
-			for (int i = 0; i < 4; i++) {
-				//현재 상태로 초기화
-				for (int j = 0; j < N; j++) {
-					mapTmp[j] = Arrays.copyOf(cur[j], N);
-				}
-				
-				//다음좌표 계산을 위해 현재좌표로 초기화
-				int nx = curCell[0];
-				int ny = curCell[1];
-				//해당 방향 가능 여부
-				boolean isPossible = false;
-				while(true) {
-					//다음 좌표 값 계산
-					nx += dx[i];
-					ny += dy[i];
-					//밖으로 나갔다면 연결 가능
-					if(!isIn(nx, ny)) {
-						isPossible = true;
-						break;
-					//장애물이 있을 경우 탐색 종료
-					} else if(mapTmp[nx][ny] != 0){
-						break;
-					}
-				}
-				
-				//연결이 가능하다면
-				if(isPossible) {
-					//다음좌표 계산을 위해 현재좌표로 초기화
-					nx = curCell[0];
-					ny = curCell[1];
-					
-					while(true) {
-						nx += dx[i];
-						ny += dy[i];
-						//해당방향 끝까지 배열에 전설을 -1로 표시하여 연결
-						if(!isIn(nx,ny)) break;
-						mapTmp[nx][ny] = -1;
-					}
-					//현재 맵 상태와 셀 하나 연결되었다고 표시 후, 다음 셀로 이동
-					find(cnt+1, mapTmp, conCell+1);
-				}
-			}
-		}
-	}
+	//그래프 길이, 외곽을 제외한 총 코어 수, 최대 코어 수, 정답(최대 코어 수일 때의 최소 전선길이)
+	private static int N, total, maxCon, answer;
+	//코어 좌표 리스트
+	private static List<int[]> coreList;
+	//프로세서 판
+	private static int[][] map;
+	//4방탐색(상우하좌)
+	private static int[] dx = {-1, 0, 1, 0};
+	private static int[] dy = {0, 1, 0, -1};
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		//입력 및 초기화
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine().trim());
 		for(int i=1; i <= T; i++) {
 			sb.append("#" + i + " ");
-			answer = Integer.MAX_VALUE;
-			maxCell = Integer.MIN_VALUE;
 			N = Integer.parseInt(br.readLine());
-			cells = new ArrayList<>();
+			total = 0;
+			maxCon = 0;
+			answer = Integer.MAX_VALUE;
+			coreList = new ArrayList<>();
 			map = new int[N][N];
 			for (int j = 0; j < N; j++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int k = 0; k < N; k++) {
 					map[j][k] = Integer.parseInt(st.nextToken());
+					//외곽에 있지 않은 코어라면 카운팅 후 좌표로 추가
 					if(map[j][k] == 1) {
-						//가장자리에 있는 것들 탐색대상에서 제외
-						if(!(j==0 || j==N-1 || k==0 || k==N-1)) cells.add(new int[] {j, k});
+						if(j>0 && k>0 && j<N-1 && k<N-1) {
+							coreList.add(new int[] {j, k});
+							total++;
+						}
 					}
 				}
 			}
-			//탐색 진행
-			find(0, map, 0);
+			//탐색시작
+			find(0, 0);
 			sb.append(answer).append("\n");
 		}
 		System.out.println(sb);
 	}
 	
+	private static void find(int cnt, int con) {
+		//현재까지 연결된 코어수 + 남은 코어 수 < 최대 연결 코어 수
+		//라면 탐색 필요없음(가지치기)
+		if(con + (total-cnt) < maxCon) return;
+		
+		//모든 코어를 탐색했다면
+		if(cnt == total) {
+			//최대 코어 수와 코어 수가 같다면 길이만 갱신
+			if(maxCon == con) {
+				//정답갱신
+				answer = Math.min(count(), answer);
+			//최대 코어수보다 현재분기의 코어 수가 크다면 갱신
+			} else if(maxCon < con) {
+				maxCon = con;
+				answer = count();
+			}
+			return;
+		}
+		
+		//현재 탐색 코어
+		int[] cur = coreList.get(cnt);
+		for (int dir = 0; dir < 4; dir++) {
+			//현재 방향 연결 가능한지 체크
+			if(isPossible(cur, dir)) {
+				//현재 코어 위치부터 해당 방향 끝까지 -1(전선)으로 표시
+				setMap(cur, dir, -1);
+				//개수 증가시키고 다음 코어 탐색
+				find(cnt+1, con+1);
+				//전선 원상복귀
+				setMap(cur, dir, 0);
+			} else {
+				//다음 코어 탐색
+				find(cnt+1 , con);
+			}
+		}
+	}
+
+	//cur 좌표부터 dir 방향으로 value로 채우는 함수
+	private static void setMap(int[] cur, int dir, int value) {
+		int nx = cur[0];
+		int ny = cur[1];
+		while(true) {
+			nx += dx[dir];
+			ny += dy[dir];
+			//외곽에 닿았다면 종료
+			if(!isIn(nx, ny)) break;
+			//value로 채우기
+			map[nx][ny] = value;
+		}	
+	}
+
+	//cur 좌표부터 dir방향으로 연결이 가능한지 확인하는 함수
+	private static boolean isPossible(int[] cur, int dir) {
+		int nx = cur[0];
+		int ny = cur[1];
+		while(true) {
+			nx += dx[dir];
+			ny += dy[dir];
+			//외곽에 닿았다면 true리턴
+			if(!isIn(nx, ny)) return true;
+			//다음 진행한 좌표가 빈칸이 아니라면 종료
+			if(map[nx][ny]!=0) break;
+		}
+		return false;
+	}
+
+	//전선 개수 카운팅 함수
+	private static int count() {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(map[i][j] == -1) cnt++; 
+			}
+		}
+		return cnt;
+	}
+	
+	//내부인지 확인하는 함수
+	private static boolean isIn(int x, int y) {
+		return x>=0 && y>=0 && x<N && y<N;
+	}
 }
