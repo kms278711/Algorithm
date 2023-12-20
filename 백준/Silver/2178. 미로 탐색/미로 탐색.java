8889cb325ed74b8cefa7b8769cafbe701269eced
kms278711 <35741985+kms278711@users.noreply.github.com>
2023-12-20 22:12:28 +0900

    [Silver I] Title: 미로 탐색, Time: 200 ms, Memory: 16780 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,72 +1,58 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 좌표 클래스
- * x좌표, y좌표, 해당 칸까지의 칸수
- */
-class Point{
-	public int x,y, value;
-	public Point(int x, int y, int value) {
-		this.x = x;
-		this.y = y;
-		this.value = value;
-	}
-}
-
 public class Main {
-	//미로 게임판
-	private static int[][] board;
-	//방문배열
-	private static boolean[][] visited;
-	//행,열
-	private static int N,M;
-	//상 우 하 좌
-	private static int[] dx = {-1, 0, 1, 0};
-	private static int[] dy = {0, 1, 0, -1};
-	private static int answer;
-	/**
-	 * 탐색함수
-	 */
-	private static void BFS() {
-		Queue<Point> queue = new ArrayDeque<>();
-		//첫칸 넣어주기
-		queue.offer(new Point(0,0,1));
-		while(!queue.isEmpty()) {
-			Point cur = queue.poll();
-			//도착했다면 break;
-			if(cur.x == N-1 && cur.y == M-1) {
-				answer = cur.value;
-				break;
-			}
-			//사방탐색
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x+dx[i];
-				int ny = cur.y+dy[i];
-				//게임판 내부인지
-				//갈 수 있는 장소인지
-				//방문 안한 곳인지
-				if(nx >= 0 && ny >= 0 && nx < N && ny < M && board[nx][ny] == 1 && !visited[nx][ny]) {
-					queue.offer(new Point(cur.x+dx[i], cur.y+dy[i], cur.value+1));
-					visited[nx][ny] = true;
-				}
-			}
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		board = new int[N][M];
-		visited = new boolean[N][M];
-		for (int i = 0; i < N; i++) {
-			String[] tmp = br.readLine().split("");
-			for (int j = 0; j < M; j++) {
-				board[i][j] = Integer.parseInt(tmp[j]);
-			}
-		}
-		BFS();
-		System.out.println(answer);
-	}
+    //가로길이, 세로길이
+    private static int N,M;
+    //맵정보
+    private static int[][] map;
+    public static void main(String[] args) throws IOException {
+        //입력 및 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for (int x = 0; x < N; x++) {
+            String[] tmp = br.readLine().split("");
+            for (int y = 0; y < M; y++) {
+                map[x][y] = Integer.parseInt(tmp[y]);
+            }
+        }
+        find();
+    }
+
+    private static void find() {
+        //int[0] : 이동횟수
+        //int[1] : 현재좌표 
+        Queue<int[][]> queue = new ArrayDeque<>();
+        queue.offer(new int[][] {{1},{0,0}});
+        //방문배열
+        boolean[][] visited = new boolean[N][M];
+        visited[0][0] = true;
+        //우하좌상
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
+        while(!queue.isEmpty()) {
+            int[][] cur = queue.poll();
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = cur[1][0] + dx[dir];
+                int ny = cur[1][1] + dy[dir];
+                if(!isIn(nx,ny)) continue;
+                if(visited[nx][ny]) continue;
+                if(map[nx][ny] == 0) continue;
+                //다음 갈 좌표가 도착좌표면 해당 칸까지의 이동횟수 출력
+                if(nx == N-1 && ny == M-1) {
+                    System.out.println(cur[0][0] + 1);
+                    return;
+                }
+                queue.offer(new int[][] {{cur[0][0]+1}, {nx,ny}});
+                visited[nx][ny] = true;
+            }
+        }
+    }
+
+    private static boolean isIn(int x, int y) {
+        return x>=0 && y>=0 && x<N && y<M;
+    }
 }
